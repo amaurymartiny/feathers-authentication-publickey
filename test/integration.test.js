c1a927bb5ced178c73af7c7fdef49c19063dd439
Amaury Martiny <amaury.martiny@gmail.com>
2017-08-24 21:55:13 +0200

    Add integration test

---
+++ test/integration.test.js
@@ -0,0 +1,66 @@
+import feathers from 'feathers';
+import authentication from 'feathers-authentication';
+import memory from 'feathers-memory';
+import hooks from 'feathers-hooks';
+import publicKey from '../src';
+import { expect } from 'chai';
+
+describe('integration', () => {
+  it('verifies', () => {
+    const User = {
+      publicKey: '0x0b664ebf14e204cde96690461830e0dd5dfb22aa',
+      nonce: 100,
+    };
+
+    // Message '100' signed by 0x0b664ebf14e204cde96690461830e0dd5dfb22aa: web3.eth.sign('0x0b664ebf14e204cde96690461830e0dd5dfb22aa', '100');
+    const correctSignature = '0x5909004173042d7b42a110c1f3325b704ca6361fc29698051955c7b002af5ffd2bcb379cc58ca331606dfed5d78f08651e20efa763e00ad9ba80a7c3397a16a81b';
+
+    const req = {
+      query: {},
+      body: {
+        id: 0,
+        signature: correctSignature,
+      },
+      headers: {},
+      cookies: {},
+      params: {
+        query: {},
+        provider: 'socketio',
+        headers: {},
+        session: {},
+        cookies: {},
+        data: 'Hello, world'
+      }
+    };
+
+    const app = feathers();
+    let paramsReceived = false;
+    let dataReceived;
+
+    app.configure(hooks())
+      .configure(authentication({ secret: 'secret' }))
+      .configure(publicKey())
+      .use('/users', memory());
+
+    app.service('users').hooks({
+      before: {
+        find: (hook) => {
+          paramsReceived = Object.keys(hook.params);
+          dataReceived = hook.params.data;
+        },
+      }
+    });
+
+    app.setup();
+
+    return app.service('users').create(User).then((response) => {
+      return app.authenticate('publicKey')(req).then(result => {
+        expect(result.success).to.equal(true);
+        expect(result.data.user.publicKey).to.equal(User.publicKey);
+        expect(result.data.user.nonce).to.equal(100);
+        expect(paramsReceived).to.have.members(['data', 'query']);
+        expect(dataReceived).to.equal('Hello, world');
+      });
+    });
+  });
+});
